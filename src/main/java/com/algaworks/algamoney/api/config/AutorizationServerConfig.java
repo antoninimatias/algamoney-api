f5bc3da576cb21611b98cb7eaa215893a40addcd
Antonini Matias <antonini.junior@gmail.com>
2018-08-01 17:29:20 -0300

    #20 - Renovando o access token com o refresh token

--- src/main/java/com/algaworks/algamoney/api/config/AutorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AutorizationServerConfig.java
@@ -21,15 +21,19 @@ public class AutorizationServerConfig extends AuthorizationServerConfigurerAdapt
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("angular").secret("@ngul@r0").scopes("read", "write")
-				.authorizedGrantTypes("password").accessTokenValiditySeconds(1800);
+		clients.inMemory().withClient("angular")
+				.secret("@ngul@r0").scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(20)
+				.refreshTokenValiditySeconds(3600 * 24);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
-		.accessTokenConverter(accessTokenConverter())
-		.authenticationManager(authenticationManager);
+				.accessTokenConverter(accessTokenConverter())
+				.reuseRefreshTokens(false)
+				.authenticationManager(authenticationManager);
 	}
 
 	@Bean
