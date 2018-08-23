e5ebe23c2effcc6826087afb2907c4aabcaa6ff0
Antonini Matias <antonini.junior@gmail.com>
2018-08-23 19:38:10 -0300

    #23 - Movendo o usuário para o banco de dados e

--- src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
@@ -19,37 +19,36 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-	private String originPermitida = "http://localhost:8000"; //TODO: Configurar para ambientes diferentes
+	private String originPermitida = "http://localhost:8000"; // TODO: Configurar para diferentes ambientes
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
-
+		
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
-
-		response.setHeader("Access-control-Allow-Origin)", originPermitida);
-		response.setHeader("Access-control-Allow-Credentials)", "true");
+		
+		response.setHeader("Access-Control-Allow-Origin", originPermitida);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
 		
 		if ("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
-			response.setHeader("Access-control-Allow-Methods)", "POST, GET, DELETE, PUT, OPTIONS");
-			response.setHeader("Access-control-Allow-Headers)", "Authorization, Content-Type, Accept");
-			response.setHeader("Access-control-Max-Ages)", "3600");
-
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        	response.setHeader("Access-Control-Max-Age", "3600");
+			
 			response.setStatus(HttpServletResponse.SC_OK);
 		} else {
 			chain.doFilter(req, resp);
 		}
+		
 	}
-
+	
 	@Override
 	public void destroy() {
-
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-
 	}
 
 }
